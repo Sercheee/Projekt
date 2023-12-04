e4bfae3f319261fda59744ad724b1eb9c84f2af4
Serchee <mrserek777@gmail.com>
2023-12-04 20:13:33 +0100

    Zadanie domowe dzień 4

---
+++ Program 1/Program 1/Program.cs
@@ -0,0 +1,51 @@
+﻿//Liczby całkowite
+var myAge = 25;
+int myAge2 = 30;
+int newAge = myAge2 + 7;
+
+int newVar1 = int.MaxValue;
+int newVar2 = int.MinValue;
+
+uint newVar3 = uint.MaxValue;
+uint newVar4 = uint.MinValue;
+
+ulong newVar5 = ulong.MaxValue;
+ulong newVar6 = ulong.MinValue;
+
+//Liczby zmiennoprzecinkowe
+float myNumber1 = float.MaxValue;
+float myNumber2 = float.MinValue;
+
+double myNumber3 = double.MaxValue;
+double myNumber4 = double.MinValue;
+
+decimal myNumber5 = decimal.MaxValue;
+decimal myNumber6 = decimal.MinValue;
+
+//Zmienne tekstowe
+string name = "Łukasz";
+string surname = "Serchee";
+char myVar = ' ';
+string result = name + myVar +surname;
+
+//Wartości logiczne
+bool isActive1 = false;
+bool isActive2 = true;
+isActive1 = false;
+var isValid = 5 > 6;
+
+Console.WriteLine(myAge);
+Console.WriteLine(newVar1);
+Console.WriteLine(newVar2);
+Console.WriteLine(newVar3);
+Console.WriteLine(newVar4);
+Console.WriteLine(newVar5);
+Console.WriteLine(newVar6);
+Console.WriteLine(myNumber1);
+Console.WriteLine(myNumber2);
+Console.WriteLine(myNumber3);
+Console.WriteLine(myNumber4);
+Console.WriteLine(myNumber5);
+Console.WriteLine(myNumber6);
+Console.WriteLine(result);
+Console.WriteLine(isValid);
